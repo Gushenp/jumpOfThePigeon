47089cd08fb84b7151d3a117a16c7d7a5406de02
Gushenp <gushenriqp861programer@gmail.com>
2026-04-05 12:56:46 -0300

    Adição de transição

--- JumpOfThePigeon/objetos/fases/scrpits/base_fundamental.gd
+++ JumpOfThePigeon/objetos/fases/scrpits/base_fundamental.gd
@@ -1,26 +1,40 @@
 extends Node2D
 
 # ==== Adição de nós =====
+@onready var player = $player
+@onready var elements = $map/elementos_globais
 @onready var normal_map = $map/normal_map
 @onready var other_map = $map/other_map
-@onready var normal_map_elementos = $map/normal_map/elementos
 @onready var camera = $player/camera
 @onready var sound_nature = $audios/Nature
 @onready var sound_Oficial2 = $audios/Oficial2
+@onready var transicao = $"transições/Transições/ColorRect"
+@onready var transicao_animation = $"transições/Transições/ColorRect/AnimationPlayer"
 
 # ===== Variáveis de controle ======
 var is_in_normal_map = true
 
 # ===== Funções de iniciação
 func _ready() -> void:
-	$player.change_reality.connect(_change_reality)
-	sound_nature.play()
-	sound_Oficial2.play()
+	_start_game()
 	pass 
 
 # ==== Funções por frame =====
 func _process(delta: float) -> void:
 	pass
+
+func _start_game():
+	_desativar_areas(elements)
+	_desativar_sons(elements)
+	_desativar_sons(player)
+	transicao_animation.play("fade_in")
+	await get_tree().create_timer(2.0).timeout
+	_reativar_areas(elements)
+	_reativar_sons(elements)
+	_reativar_sons(player)
+	$player.change_reality.connect(_change_reality)
+	sound_nature.play()
+	sound_Oficial2.play()
 
 # ==== Mudar realidade =====
 func _change_reality():
@@ -37,21 +51,72 @@ func _change_reality():
 func _desativar_realidade(node):
 	node.get_node("tileMap").hide() #visual - tilemap 
 	node.get_node("tileMap/solo").enabled = false
-	_desativar_objetos(node)
 	
 func _reativar_realidade(node):
 	node.get_node("tileMap").show() #visual - tilemap
 	node.get_node("tileMap/solo").enabled = true
-	_reativar_objetos(node)
-	
-func _desativar_objetos(node):
+
+# == Controle colisoes ==
+func _desativar_collisoes(node):
 	if node == normal_map:
 		pass
 
-func _reativar_objetos(node):
+func _reativar_collisoes(node):
 	if node == normal_map:
 		pass
 		
+
+# == Controle areas 2d ==
+func _desativar_areas(node):
+	for child in node.get_children():
+		_desativar_areas_recursivo(child)
+		
+
+func _desativar_areas_recursivo(node):
+	if node is Area2D:
+		node.monitoring = false
+		node.monitorable = false
+		
+	for child in node.get_children():
+		_desativar_areas_recursivo(child)
+		
+func _reativar_areas(node):
+	for child in node.get_children():
+		_reativar_areas_recursivo(child)
+		
+
+func _reativar_areas_recursivo(node):
+	if node is Area2D:
+		node.monitoring = true
+		node.monitorable = true
+		
+	for child in node.get_children():
+		_reativar_areas_recursivo(child)
+		
+#== Controle de sons ==
+func _desativar_sons(node):
+	for child in node.get_children():
+		_desativar_sons_recursivo(child)
+		
+
+func _desativar_sons_recursivo(node):
+	if node is AudioStreamPlayer:
+		node.volume_db = -200
+		
+	for child in node.get_children():
+		_desativar_sons_recursivo(child)
+		
+func _reativar_sons(node):
+	for child in node.get_children():
+		_reativar_sons_recursivo(child)
+		
+
+func _reativar_sons_recursivo(node):
+	if node is AudioStreamPlayer:
+		node.volume_db = 0
+		
+	for child in node.get_children():
+		_reativar_sons_recursivo(child)
 #=== Funções da camera ====
 
 func _on_zoom_1_body_entered(body: CharacterBody2D) -> void:
@@ -84,3 +149,10 @@ func _zoom_camera(alvo1, alvo2):
 	tween.tween_property(camera, "zoom", Vector2(alvo1, alvo2), 0.5)\
 		.set_trans(Tween.TRANS_SINE)\
 		.set_ease(Tween.EASE_IN_OUT)
+
+# == Saida ===
+func _on_saída_body_entered(body: CharacterBody2D) -> void:
+	transicao_animation.play("fade_out")
+	await get_tree().create_timer(2.0).timeout
+	get_tree().change_scene_to_file("res://JumpOfThePigeon/Worlds/World1/Levels/level_2.tscn")
+	pass 
